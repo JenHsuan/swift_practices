8249f2d60419ca5595fa5b2c12b427c6c1c23861
Sean <of_alpha@hotmail.com>
2022-10-22 21:40:43 +0800

    exercise 1.1

---
+++ playgrounds/exercise_1_1.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+let day = "Monday"
+let dailyTemperature = 75
+print("Today is \(day)")
+print("The temperature on \(day) is \(dailyTemperature)°F.")
+
+var temperature = 70
+print("The temperature on \(day) is \(temperature)°F.")
+
+temperature = 80
+print("The temperature on \(day) is \(temperature)°F.")
+
+let weeklyTemperature = 75
+temperature = weeklyTemperature
+print("The average temperature on \(day) is \(temperature)°F.")
+
