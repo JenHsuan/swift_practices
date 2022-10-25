80a2c043c84a42e2dbc7d6f04c35c9023540e074
Sean <of_alpha@hotmail.com>
2022-10-25 20:38:07 +0800

    Add the exercise for functions (advance)

---
+++ playgrounds/exercise_3_function_advance.playground/Contents.swift
@@ -0,0 +1,15 @@
+var goldBars = 0
+incrementInventory(&goldBars)
+print(goldBars)
+incrementInventory(&goldBars)
+print(goldBars)
+incrementInventory(&goldBars)
+print(goldBars)
+incrementInventory(&goldBars, by: 300)
+print(goldBars)
+incrementInventory(&goldBars, by: 50)
+print(goldBars)
+
+func incrementInventory(_ inventory: inout Int, by amount : Int = 100) {
+    inventory += amount
+}
