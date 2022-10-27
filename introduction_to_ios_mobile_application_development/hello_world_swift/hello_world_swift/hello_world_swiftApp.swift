e1ad8b10eb8522b4764aa02a6e7ff51383b6a344
Sean Hsieh <hsiehs@fortinet.com>
2022-10-27 11:26:37 +0800

    Init the project for Swift

---
+++ introduction_to_ios_mobile_application_development/hello_world_swift/hello_world_swift/hello_world_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_world_swiftApp.swift
+//  hello_world_swift
+//
+//  Created by Sean Hsieh on 2022/10/27.
+//
+
+import SwiftUI
+
+@main
+struct hello_world_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
